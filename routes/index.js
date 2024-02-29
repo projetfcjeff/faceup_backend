a5e9deb8a7ab33702d9b9b98f25f9783ffdb928a
jfbonglet <jean.francois.bonglet@gmail.com>
2024-02-29 14:49:10 +0100

    update

--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,22 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-const cloudinary = require('cloudinary').v2;
-const fs = require('fs');
-const uniqid = require('uniqid');
+const cloudinary = require("cloudinary").v2;
+const fs = require("fs");
+const uniqid = require("uniqid");
 
-router.post('/upload', async (req, res) => {
-    const photoPath = `./tmp/${uniqid()}.jpg`;
-    const resultMove = await req.files.photoFromFront.mv(photoPath);
-    
-   
-    if(!resultMove) 
-    {
-        const resultCloudinary = await cloudinary.uploader.upload(photoPath);
-        fs.unlinkSync(photoPath);
-        res.json({ result: true, url: resultCloudinary.secure_url });      
-    } else 
-    {
-        res.json({ result: false, error: resultCopy });
-    }
-   });
+router.post("/upload", async (req, res) => {
+  const photoPath = `./tmp/${uniqid()}.jpg`;
+  const resultMove = await req.files.photoFromFront.mv(photoPath);
+
+  if (!resultMove) {
+    const resultCloudinary = await cloudinary.uploader.upload(photoPath);
+    console.log(resultCloudinary);
+    fs.unlinkSync(photoPath);
+    return res.json({ result: true, url: resultCloudinary.secure_url });
+  } else {
+    return res.json({ result: false, error: resultCopy });
+  }
+});
 
 module.exports = router;
-
-
-
-
