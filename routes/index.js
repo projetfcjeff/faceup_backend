d7b32f3bb355ce66bf6d0c90437e00aa9b399247
jfbonglet <jean.francois.bonglet@gmail.com>
2024-02-29 15:36:49 +0100

    enleve . du photopath

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const fs = require("fs");
 const uniqid = require("uniqid");
 
 router.post("/upload", async (req, res) => {
-  const photoPath = `./tmp/${uniqid()}.jpg`;
+  const photoPath = `/tmp/${uniqid()}.jpg`;
   const resultMove = await req.files.photoFromFront.mv(photoPath);
 
   if (!resultMove) {
